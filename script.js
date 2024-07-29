a76a28a020d2986b18948387aa48f87e758c384c
SaiefB <A.S.Bokor@outlook.com>
2024-07-29 13:53:13 +0100

    Capitalize Excercise

--- script.js
+++ script.js
@@ -18,3 +18,10 @@ console.log(multiplyResult);
 
 
 
+function capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase()
+}
+
+let capitalizeResult = capitalize("HeLlO");
+
+console.log(capitalizeResult);
