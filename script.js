5d4703cfa5bbd072e502fb9b38521eafccd7c939
SaiefB <A.S.Bokor@outlook.com>
2024-07-29 13:52:37 +0100

    Multiply excercise

--- script.js
+++ script.js
@@ -5,3 +5,16 @@ function add7(a) {
 let result = add7(4);
 
 console.log(result);
+
+
+
+function multiply(a, b) {
+    return a * b
+};
+
+let multiplyResult = multiply(10, 5);
+
+console.log(multiplyResult);
+
+
+
